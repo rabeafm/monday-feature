c1ba7a6e9781252d13807c7ae0cc6e55d2f31d89
Rabeafm <rabeaf@moveo.co.il>
2021-11-08 16:57:01 +0200

    api manipulation

--- src/App.js
+++ src/App.js
@@ -1,41 +1,50 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import mondaySdk from 'monday-sdk-js';
-import 'monday-ui-react-core/dist/main.css';
 //Explore more Monday React Components here: https://style.monday.com/
-import AttentionBox from 'monday-ui-react-core/dist/AttentionBox.js';
-import Item from './components/Item';
+import { Routes, Route } from 'react-router-dom';
+import 'monday-ui-react-core/dist/main.css';
+import mondaySdk from 'monday-sdk-js';
+import React, { useEffect, useState } from 'react';
+import './App.css';
 import Board from './components/Board';
+import Item from './components/Item';
 
 const monday = mondaySdk();
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [state, setState] = useState({
+    settings: {},
+    context: {},
+  });
+  const [item, setItem] = useState();
 
-    // Default state
-    this.state = {
-      settings: {},
-      name: '',
-    };
-  }
+  useEffect(() => {
+    monday.listen('context', (res) => {
+      setState({ settings: state.settings, context: res.data });
+    });
+  }, []);
 
-  componentDidMount() {
-    // TODO: set up event listeners
-  }
-
-  render() {
-    return (
-      <Routes>
-        <Route path="/item" element={<Item />} />
-        <Route path="/board" element={<Board />} />
-      </Routes>
-    );
-  }
-}
+  useEffect(() => {
+    if (state.context.itemId) {
+      monday
+        .api(
+          `query ($itemIds: [Int]) { items (ids: $itemIds) *`, //{ name id  column_values {
+          //title : 'Timeline'
+          //}  } }`,
+          {
+            variables: {
+              itemIds: [state.context.itemId],
+            },
+          }
+        )
+        .then((res) => setItem(res.data));
+    }
+  }, [state]);
+  console.log('rendered:', item);
+  return (
+    <Routes>
+      <Route path="/item" element={<Item {...state} />} />
+      <Route path="/board" element={<Board />} />
+    </Routes>
+  );
+};
 
 export default App;
-{
-  /*  */
-}
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,16 +1,17 @@
 import AttentionBox from 'monday-ui-react-core/dist/AttentionBox.js';
+import React from 'react';
 
-import React, { Fragment } from 'react';
-const Item = () => {
+const Item = (props) => {
+  console.log(props);
   return (
-    <Fragment>
+    <div>
       <h1>Item</h1>
       <AttentionBox
-        title="Hello Monday Apps!"
-        text="Let's start building your amazing app, which will change the world!"
+        title="Hello  Apps!"
+        text={JSON.stringify(props, null, 2)}
         type="success"
       />
-    </Fragment>
+    </div>
   );
 };
 
