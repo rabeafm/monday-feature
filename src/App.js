3f56e24bd7ccd90a79eb43093bba9814056af079
Sagi <sagidmoveo.group>
2021-11-09 10:17:27 +0200

    first api request

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import mondaySdk from 'monday-sdk-js';
-import 'monday-ui-react-core/dist/main.css';
+import React, { Fragment } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import mondaySdk from "monday-sdk-js";
+import "monday-ui-react-core/dist/main.css";
 //Explore more Monday React Components here: https://style.monday.com/
-import AttentionBox from 'monday-ui-react-core/dist/AttentionBox.js';
-import Item from './components/Item';
-import Board from './components/Board';
+import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js";
+import Item from "./components/Item";
+import Board from "./components/Board";
 
 const monday = mondaySdk();
 
@@ -17,7 +17,7 @@ class App extends React.Component {
     // Default state
     this.state = {
       settings: {},
-      name: '',
+      name: "",
     };
   }
 
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,16 +1,70 @@
-import AttentionBox from 'monday-ui-react-core/dist/AttentionBox.js';
+import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js";
+import React, { Fragment, useState, useEffect } from "react";
+import mondaySdk from "monday-sdk-js";
 
-import React, { Fragment } from 'react';
+// id: "1879880968"
+// name: "Test Board"
+// { groups {title id} }
+
+// query {boards (ids: 1879880968) {
+//   items{
+//     id
+//     name
+//     column_values {
+//       Actual time
+//     }
+
+// } } }
+
+// items {
+//   column_values {
+//   text
+//   title
+//   id
+//         }
+//       }
+
+const monday = mondaySdk();
+
 const Board = () => {
+  const [boards, setBoards] = useState();
+
+  const generateBoard = async () => {
+    try {
+      monday.setToken(
+        "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjEzMjA1NTk4NywidWlkIjoyNTAzNjQ1NCwiaWFkIjoiMjAyMS0xMS0wOFQxMjoxMjo0OS4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTEwMjMyOCwicmduIjoidXNlMSJ9.fcTHFIXYVCOZsbU0hIaknPPpOX-pVsXGXg5qkoVEKkk"
+      );
+      const res = await monday.api(`query {
+        boards(ids: 1879880968) {
+          groups {
+            title
+            items {
+              id
+            }
+            
+          }
+        }
+      }
+        `);
+
+      setBoards(res);
+      console.log(res.data);
+      console.log("res.data");
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    generateBoard();
+  }, []);
+
   return (
     <Fragment>
       <h1>Board</h1>
-      <AttentionBox
-        title="Hello Monday Apps!"
-        text="Let's start building your amazing app, which will change the world!"
-        type="success"
-      />
+      <AttentionBox title="Hello Monday Apps!" text="" type="success" />
     </Fragment>
   );
 };
+
 export default Board;
